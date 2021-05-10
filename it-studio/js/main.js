dc522e0dd4a55842a7fb2ca7904c7ba31af44d33
Xenia <cocraren359@gmail.com>
2021-05-10 17:51:29 +0400

    del

--- it-studio/js/main.js
+++
@@ -1,8 +0,0 @@
-const navList = document.querySelector('.nav__list'),
-        mobileBtn = document.querySelector('.mobile__menu');
-
-    mobileBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        navList.classList.toggle('show');
-    });
